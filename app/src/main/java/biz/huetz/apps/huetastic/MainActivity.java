6c26a606ebdc23157aeecdcd07079fc302429945
Tim Hütz <tim@huetz.biz>
2016-09-27 12:15:04 +0200

    Remove some sample code

--- app/src/main/java/biz/huetz/apps/huetastic/MainActivity.java
+++ app/src/main/java/biz/huetz/apps/huetastic/MainActivity.java
@@ -22,14 +22,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 		Toolbar toolbar = (Toolbar) findViewById( R.id.toolbar );
 		setSupportActionBar( toolbar );
 
-		FloatingActionButton fab = (FloatingActionButton) findViewById( R.id.fab );
-		fab.setOnClickListener( new View.OnClickListener() {
-			@Override
-			public void onClick( View view ) {
-				Snackbar.make( view, "Replace with your own action", Snackbar.LENGTH_LONG ).setAction( "Action", null ).show();
-			}
-		} );
-
 		DrawerLayout drawer = (DrawerLayout) findViewById( R.id.drawer_layout );
 		ActionBarDrawerToggle toggle = new ActionBarDrawerToggle( this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close );
 		drawer.setDrawerListener( toggle );
@@ -51,16 +43,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
 	@Override
 	public boolean onCreateOptionsMenu( Menu menu ) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate( R.menu.main, menu );
 		return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected( MenuItem item ) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 
 		//noinspection SimplifiableIfStatement
@@ -74,11 +62,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 	@SuppressWarnings( "StatementWithEmptyBody" )
 	@Override
 	public boolean onNavigationItemSelected( MenuItem item ) {
-		// Handle navigation view item clicks here.
 		int id = item.getItemId();
 
 		if( id == R.id.nav_camera ) {
-			// Handle the camera action
+
 		} else if( id == R.id.nav_share ) {
 
 		} else if( id == R.id.nav_send ) {
